67c7d4957de062bd9ad2c7a5d86bb10f8ed7fa62
lepakko <hensch0r@gmx.de>
2025-09-21 20:59:42 +0200

    Update to fetch Spieltage from new sheet using pub URLs, others from old sheet using gviz

--- src/App.jsx
+++ src/App.jsx
@@ -15,20 +15,23 @@ import './App.css';
 // Die Google Sheet ID deines Dokuments
 const GOOGLE_SHEET_ID = '1hzAceouOYAj7_cHMRuJQTIXCnJ9RsaBydW8g_lPK8I8';
 
+// Neue Sheet ID für Spieltage
+const NEW_GOOGLE_SHEET_ID = '2PACX-1vRSt6yVrxnSDV5YC3gzAjCsnTmXfab2DnB0fyMa-NFN0emZUXypj-0nIYTcT42XwVIMjLpr2O2PBCWA';
+
 // Liste mit GIDs für jeden Spieltag
 const MATCHDAY_GIDS = {
   "1": "2006878690",
-  "2": "1100254037",
-  "3": "807924221",
-  "4": "28187050",
-  "5": "1387863898",
-  "6": "955094077",
-  "7": "96908954",
-  "8": "2004360002",
-  "9": "777935510",
-  "10": "782646271",
-  "11": "1260966685",
-  "12": "1182183385",
+  "2": "1569336654",
+  "3": "893149680",
+  "4": "2131901049",
+  "5": "157358606",
+  "6": "122140122",
+  "7": "1574698297",
+  "8": "1357659623",
+  "9": "1435566274",
+  "10": "141598633",
+  "11": "883734387",
+  "12": "1395746189",
 };
 
 // Korrekte GIDs für deine Hauptmappen
@@ -39,8 +42,11 @@ const GOOGLE_SHEET_GIDS = {
   team: "294324544",
 };
 
-const fetchData = async (gid) => {
-  const url = `https://docs.google.com/spreadsheets/d/${GOOGLE_SHEET_ID}/gviz/tq?tqx=out:csv&gid=${gid}`;
+const fetchData = async (gid, sheetId = GOOGLE_SHEET_ID) => {
+  const isNewSheet = sheetId === NEW_GOOGLE_SHEET_ID;
+  const url = isNewSheet 
+    ? `https://docs.google.com/spreadsheets/d/e/${sheetId}/pub?gid=${gid}&single=true&output=csv`
+    : `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&gid=${gid}`;
   const response = await fetch(url);
   const csvText = await response.text();
   return Papa.parse(csvText, { header: true, dynamicTyping: true }).data;
@@ -60,16 +66,18 @@ function App() {
     try {
       setData((prev) => ({ ...prev, loading: true }));
 
-      const matchdayPromises = Object.values(MATCHDAY_GIDS).map(gid => fetchData(gid));
+      const matchdayPromises = Object.values(MATCHDAY_GIDS).map(gid => fetchData(gid, NEW_GOOGLE_SHEET_ID));
       const allMatchdayData = await Promise.all(matchdayPromises);
       const flattenedMatchdays = allMatchdayData.flat();
 
       const [playersData, ligaData, starterData, teamData] = await Promise.all([
-        fetchData(GOOGLE_SHEET_GIDS.players),
-        fetchData(GOOGLE_SHEET_GIDS.liga),
-        fetchData(GOOGLE_SHEET_GIDS.starter),
-        fetchData(GOOGLE_SHEET_GIDS.team),
+        fetchData(GOOGLE_SHEET_GIDS.players, GOOGLE_SHEET_ID),
+        fetchData(GOOGLE_SHEET_GIDS.liga, GOOGLE_SHEET_ID),
+        fetchData(GOOGLE_SHEET_GIDS.starter, GOOGLE_SHEET_ID),
+        fetchData(GOOGLE_SHEET_GIDS.team, GOOGLE_SHEET_ID),
       ]);
+
+      console.log('Fetched data:', { playersData, ligaData, starterData, teamData, flattenedMatchdays });
 
       const matchdays = processMatchdays(flattenedMatchdays, starterData);
       const playerStats = calculatePlayerStats(flattenedMatchdays, playersData, starterData);
